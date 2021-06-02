bac1f69af40a621122b22f5fe8b138dcbb31ae1b
suricata3838 <summer970827@gmail.com>
2021-06-03 02:10:29 +0900

    Customize for cerberus

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,31 +3,28 @@ import AnchorLink from 'react-anchor-link-smooth-scroll';
 import LogoIcon from '../../svg/LogoIcon';
 import Button from '../Button';
 
+let url="https://kero.substack.com/p/coming-soon";
 const Header = () => (
   <header className="sticky top-0 bg-white shadow">
     <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
-      <div className="flex items-center text-2xl">
-        <div className="w-12 mr-3">
-          <LogoIcon />
+      <div className="flex items-center">
+        <div className="w-12 mr-1">
+          <img src="cerberus.png" width="142" height="142"/>
         </div>
-        Lander
       </div>
-      <div className="flex mt-4 sm:mt-0">
-        <AnchorLink className="px-4" href="#features">
+      <div className="flex mt-3 sm:mt-0">
+        <AnchorLink className="px-3" href="#features">
           Features
         </AnchorLink>
-        <AnchorLink className="px-4" href="#services">
+        <AnchorLink className="px-3" href="#services">
           Services
         </AnchorLink>
-        <AnchorLink className="px-4" href="#stats">
+        <AnchorLink className="px-3" href="#stats">
           Stats
-        </AnchorLink>
-        <AnchorLink className="px-4" href="#testimonials">
-          Testimonials
         </AnchorLink>
       </div>
       <div className="hidden md:block">
-        <Button className="text-sm">Start Free Trial</Button>
+        <Button className="text-sm"><a style={{ color: "white" }} href={url}>Subscribe</a></Button>
       </div>
     </div>
   </header>
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,25 +10,21 @@ import customerData from '../data/customer-data';
 import HeroImage from '../svg/HeroImage';
 import SvgCharts from '../svg/SvgCharts';
 
+let url="https://kero.substack.com/p/coming-soon";
 const Index = () => (
   <Layout>
     <section className="pt-20 md:pt-40">
-      <div className="container mx-auto px-8 lg:flex">
-        <div className="text-center lg:text-left lg:w-1/2">
+      <div className="container mx-auto">
+        <div className="text-center">
           <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
-            Main title of your landing page
+            We are Cerberus Research, Inc. 
           </h1>
           <p className="text-xl lg:text-2xl mt-6 font-light">
-            Free landing page template to promote your business startup and generate leads for the
-            offered services
+            We'll serve the significat insight about DeFi market.
           </p>
           <p className="mt-8 md:mt-12">
-            <Button size="lg">Get Started</Button>
+            <Button size="lg"><a style={{ color: "white" }} href={url}>Subscribe</a></Button>
           </p>
-          <p className="mt-4 text-gray-600">Sed fermentum felis ut cursu</p>
-        </div>
-        <div className="lg:w-1/2">
-          <HeroImage />
         </div>
       </div>
     </section>
@@ -40,8 +36,7 @@ const Index = () => (
             <Card className="mb-8">
               <p className="font-semibold text-xl">Service One</p>
               <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
+                DeFi Market Analysis
               </p>
             </Card>
           </div>
@@ -49,8 +44,7 @@ const Index = () => (
             <Card className="mb-8">
               <p className="font-semibold text-xl">Service Two</p>
               <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
+                PaperTrading Simulation Engine on DeFi 
               </p>
             </Card>
           </div>
@@ -58,8 +52,7 @@ const Index = () => (
             <Card className="mb-8">
               <p className="font-semibold text-xl">Service Three</p>
               <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
+                Advertisement and Token Salse, etc.
               </p>
             </Card>
           </div>
@@ -70,7 +63,7 @@ const Index = () => (
       id="services"
       primarySlot={
         <div className="lg:pr-32 xl:pr-48">
-          <h3 className="text-3xl font-semibold leading-tight">Market Analysis</h3>
+          <h3 className="text-3xl font-semibold leading-tight">DeFi Market Analysis</h3>
           <p className="mt-8 text-xl font-light leading-relaxed">
             Our team of enthusiastic marketers will analyse and evaluate how your company stacks
             against the closest competitors
@@ -124,25 +117,13 @@ const Index = () => (
         </div>
       </div>
     </section>
-    <section id="testimonials" className="py-20 lg:py-40">
-      <div className="container mx-auto">
-        <LabelText className="mb-8 text-gray-600 text-center">What customers are saying</LabelText>
-        <div className="flex flex-col md:flex-row md:-mx-3">
-          {customerData.map(customer => (
-            <div key={customer.customerName} className="flex-1 px-3">
-              <CustomerCard customer={customer} />
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
     <section className="container mx-auto my-20 py-24 bg-gray-200 rounded-lg text-center">
-      <h3 className="text-5xl font-semibold">Ready to grow your business?</h3>
+      <h3 className="text-5xl font-semibold">Don't miss our update!</h3>
       <p className="mt-8 text-xl font-light">
-        Quis lectus nulla at volutpat diam ut. Enim lobortis scelerisque fermentum dui faucibus in.
+        We'll provive our update in a newletter. 
       </p>
       <p className="mt-8">
-        <Button size="xl">Get Started Now</Button>
+        <Button size="xl"><a style={{ color: "white" }} href={url}>Subscribe</a></Button>
       </p>
     </section>
   </Layout>
